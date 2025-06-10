bf2793da31a57a89cd34027a0654d1e0a1c6e1bf
RenatoMac <renato.deimarini@gmail.com>
2025-06-10 23:50:07 +0200

    ex02

--- sd00/ex02/segmented_runs.c
+++ sd00/ex02/segmented_runs.c
@@ -1,8 +1,25 @@
-/* 
-* Find segment
-* Check if is valid segment
-*
-*/
+#include "segmented_runs.h"
+
+int	is_valid(const int *arr, int start, int end)
+{
+	int	count = 0;
+
+	if (start >= end)
+		return (0);
+	while (arr[start] == -1)
+		start++;
+	while (start < end)
+	{
+		if (arr[start] < arr[start + 1] && arr[start] != -1)
+			count++;
+		else
+			count = 0;
+		if (count > 1)
+			return (1);
+		start++;
+	}
+	return (0);
+}
 
 int	count_segments(const int *arr, int size)
 {
@@ -14,10 +31,35 @@ int	count_segments(const int *arr, int size)
 	{
 		if (arr[i] == -1)
 		{
-			if (is_valid())//TODO
+			if (is_valid(arr, start, i))
 				count++;
-			start = i;
+			start = i + 1;
 		}
 		i++;
 	}
+	if (start < size && is_valid(arr, start, size))
+		count++;
+	return (count);
 }
+
+/* int main(int ac, char **av)
+{
+	if (ac < 2)
+	{
+		printf("Usage: %s <array of integers>\n", av[0]);
+		return (1);
+	}
+
+	int arr[100];
+	int size = 0;
+
+	for (int i = 1; i < ac; i++)
+	{
+		arr[size++] = atoi(av[i]);
+	}
+
+	int segments = count_segments(arr, size);
+	printf("Number of valid segments: %d\n", segments);
+
+	return (0);
+} */
