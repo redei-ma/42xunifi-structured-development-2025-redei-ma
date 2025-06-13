e136b212979c659597d9f9347a8997e4f2b48166
Renato Dei Marini <redei-ma@c2r2p4.42firenze.it>
2025-06-13 12:02:32 +0200

    ex08

--- sd00/ex08/password_validator.h
+++ sd00/ex08/password_validator.h
@@ -14,10 +14,16 @@ typedef enum
 typedef struct
 {
 	char	*history[3];	// Array of previous passwords
-	int		last_index;	// Index of the last password
+	int		last_index;		// Index of the last password in history (-1 if empty)
 } PasswordHistory;
 
 
+int			ft_strlen(const char *str);
+int			ft_isupper(int c);
+int			ft_islower(int c);
+int			ft_isspecial(int c);
+int			ft_isdigit(int c);
+int			ft_strcmp(const char *s1, const char *s2);
 
 PwStatus	validate_password(const char *new_pw, PasswordHistory *history);
 
--- sd00/ex08/password_validator_bonus.c
+++ sd00/ex08/password_validator_bonus.c
@@ -1,7 +1,55 @@
 #include "password_validator.h"
 
-static int	ft_issimilar(const char *new_pw, struct PasswordHistory *history)
-{}
+static int ft_min(int a, int b, int c)
+{
+	int min_val = a;
+	if (b < min_val)
+		min_val = b;
+	if (c < min_val)
+		min_val = c;
+	return (min_val);
+}
+
+static int edit_distance(const char *s1, const char *s2)
+{
+	int len1 = ft_strlen(s1);
+	int len2 = ft_strlen(s2);
+	int dp[len1 + 1][len2 + 1];
+
+	for (int i = 0; i <= len1; i++)
+		dp[i][0] = i;
+	for (int j = 0; j <= len2; j++)
+		dp[0][j] = j;
+
+	for (int i = 1; i <= len1; i++)
+	{
+		for (int j = 1; j <= len2; j++)
+		{
+			if (s1[i-1] == s2[j-1])
+				dp[i][j] = dp[i-1][j-1];
+			else
+				dp[i][j] = 1 + ft_min(
+					dp[i-1][j],
+					dp[i][j-1],
+					dp[i-1][j-1]
+				);
+		}
+	}
+	return dp[len1][len2];
+}
+
+static int ft_issimilar(const char *new_pw, PasswordHistory *history)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		if (history->history[i] && history->history[i][0] != '\0')
+		{
+			if (edit_distance(new_pw, history->history[i]) <= 1)
+				return (1);
+		}
+	}
+	return (0);
+}
 
 static void	add_to_history(const char *new_pw, PasswordHistory *history, int last_index)
 {
